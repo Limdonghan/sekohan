2166bba929e842f9db5397e398610765f928dddf
LimDongHan <dim95985@gmail.com>
2023-09-27 16:45:40 +0900

    footers

--- sekohan-front/src/views/Home.vue
+++ sekohan-front/src/views/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <HeaderBar />
+
+  
+  <Footers />
 </template>
 
 <script setup>  
 import HeaderBar from '@/components/HeaderBar.vue'
+import Footers from '@/components/Footers.vue';
 
 
 </script>
