c834a20b920a830f951488219f9d65f43c673261
LimDongHan <dim95985@gmail.com>
2023-10-11 14:17:51 +0900

    10/11

--- sekohan-front/src/views/Home.vue
+++ sekohan-front/src/views/Home.vue
@@ -1,15 +1,14 @@
 <template>
-    <HeaderBar />
-    <Content />
-    <Footers />
-  </template>
-  
-  <script setup>
-  import Footers from '@/components/Footers.vue';
-import HeaderBar from '@/components/HeaderBar.vue'
-  import product from '@/components/product.vue'
-import Content from '@/main_menu/content.vue';
-  </script>
-  
-  <style>
-  </style>
+  <HeaderBar />
+  <Content />
+  <Footers />
+</template>
+
+<script setup>
+import Footers from "@/components/Footers.vue";
+import HeaderBar from "@/components/HeaderBar.vue";
+import product from "@/components/product.vue";
+import Content from "@/main_menu/content.vue";
+</script>
+
+<style></style>
